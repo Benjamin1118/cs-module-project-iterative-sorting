d32386fae70cf93f97c879511fd5068cf180a792
Benjamin Bishop <benjaminbishop12@gmail.com>
2020-07-22 13:38:12 -0600

    finished assignment

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -9,6 +12,23 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = left + right // 2
+
+        #check if mid is target
+        if arr[mid] == target:
+            return mid
+        #if target is greater ignore left
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1 #not found
+
 
 
     return -1  # not found
